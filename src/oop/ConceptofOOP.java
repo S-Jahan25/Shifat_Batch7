c3db23a9a04b5066f3492b374baf94f4b57e6222
Shifat Jahan <subojahan94@gmail.com>
2023-12-04 00:44:34 -0500

    My first commit

---
+++ src/oop/ConceptofOOP.java
@@ -0,0 +1,27 @@
+/*
+Author name : shifat jahan
+date: 11/26/2023
+
+project details:
+Object Oriented Programing:
+
+1. Inheritance : there are few types of inheritance
+    i. single inheritance = class B extends class A
+    ii. multilevel inheritance = ClassA extends  classB , and classC  extends classB.
+    iii. heirachial inheritance. : one parents and 2 child . child get properties by default from parents.
+
+2. Encapsulation
+3. Abstraction
+4. Interface
+5. polymorphism
+      i. Overloading = using same same method with different parameters
+      ii. Overriding = using child class extendes @override we can do it.
+
+
+
+ */
+
+package oop;
+
+public class ConceptofOOP {
+}
---
+++ src/oop/polymorphismDemo/MethodOverridingExample.java
@@ -0,0 +1,31 @@
+/*
+        Whar is method orverriding: Its a proccess that declaring method in subclass which already
+        present in Superclass .
+        why do we use this: to resure code and one interface with multiple interface.
+        run time polymorphism
+
+        Some rules: name, signature type and parameter must be same.
+
+        overriding = runtime polymorphism
+
+ */
+
+
+package oop.polymorphismDemo;  // package name
+
+public class MethodOverridingExample {  // class name
+    // instance variable declared in the class
+
+    String name;
+    int age;
+
+    void displayInformation(){  // custom method
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+
+    }
+
+
+
+
+}
